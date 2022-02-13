1c220c2391e3ac43192c6c2c36d21bf8f9aac60b
Aliya <aliya.bermaganbet@alumni.nu.edu.kz>
2022-02-13 11:37:11 +0300

    My second addings

--- final.c
+++ final.c
@@ -4,5 +4,7 @@ int main()
  printf("Hello,world!");
  printf("Hello,my first program!");
  printf("Hello,my second program!");
+ printf("Hello,my third program!");
+ printf("Hello,my fourth program!");
  return 0;
 }
